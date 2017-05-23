80cf22d3f2c8c551d1d4e8d49f74d4853f5ed803
Luuk van Oijen <lazyluuk.channel@gmail.com>
2017-05-23 21:06:44 +0200

    Update maingame.py

--- maingame.py
+++ maingame.py
@@ -17,33 +17,45 @@ camx = 0
 camy = 0
 
 #-----------------------#
+#-     Functions       -#
+#-----------------------#
+
+
+#-----------------------#
 #-       Classes       -#
 #-----------------------#
-class Object() :
-    def __init__(self,x,y,filename) :
-        self.x = x
-        self.y = y
-        self.file = tk.PhotoImage(file=filename)
-
-    def draw(self) :
-        global camx, camy
-        w.create_image(self.x-camx,self.y-camy,image=self.file)
-
 class Map() :
-    def __init__(self,x,y,filename,name) :
+    def __init__(self,x,y,filename,name,size=(1024,512)) :
         self.x = x
         self.y = y
         self.file = tk.PhotoImage(file=filename)
         self.name = name
+        self.size = size
 
     def draw(self) :
-        global camx, camy
-        w.create_image(self.x-camx,self.y-camy,image=self.file)
+        w.create_image(self.x+self.size[0]/2,self.y+self.size[1]/2,image=self.file)
 
-#-----------------------#
-#-     Functions       -#
-#-----------------------#
+class Object() :
+    def __init__(self,x,y,filename,name,size=(64,64)) :
+        self.x = x
+        self.y = y
+        self.file = tk.PhotoImage(file=filename)
+        self.name = name
+        self.size = size
 
+    def draw(self) :
+        w.create_image(self.x+self.size[0]/2,self.y+self.size[1]/2,image=self.file)
+
+class Player() :
+    def __init__(self,x,y,name) :
+        self.x = x
+        self.y = y
+        self.name = name
+        self.file = tk.PhotoImage(file="textures/basic_player.gif")
+
+    def draw(self) :
+        w.create_image(self.x+32,self.y+32,image=self.file)
+        w.create_text(self.x+32,self.y-12,text=self.name)
 
 #-----------------------#
 #-       Setup         -#
@@ -56,13 +68,14 @@ w.pack()
 #-        Code         -#
 #-----------------------#
 
-player = Player(0,0,input("Name: "))
-#stone001 = Object(0,0,"stone.gif")
+player = Player(0,20,input("Name: "))
+#stone001 = Object(0,0,"stone.gif","stone001")
 map_main = Map(0,0,"maps/main_map.gif","main_map")
 
 def gameloop() :
     w.delete("all")
     map_main.draw()
+    player.draw()
     w.after(1,gameloop)
 
 gameloop()
