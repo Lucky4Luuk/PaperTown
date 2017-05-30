f35865d8d8d1dfc2c42cbaf4e724387409614544
Luuk van Oijen <lazyluuk.channel@gmail.com>
2017-05-30 10:46:46 +0200

    Add files via upload

--- IBS.py
+++ IBS.py
@@ -6,5 +6,18 @@ def getItem(player,itemname) :
         return "{1} already has {0}".format(itemname,player.getName())
 
 def Attack(player,enemy) :
-    if player.getVar("attack") == True :
-        enemy.setVar("health", enemy.getVar("health") - player.getVar("dmg"))
+    if player.getAttack() == True :
+        enemy.setHealth(, enemy.getHealth() - player.getDmg())
+
+def Heal(player,health) :
+    if player.getHeal() == True :
+        player.setHealth(, player.getHealth() + player.getRegen())
+
+def Exp(player,lvl,enemy,battle) :
+    if enemy.getHealth() <= 0 :
+        battle = False
+        player.setLvl(, player.getLvl() + enemy.getExp())
+
+
+def UseItem(player,itemname) :
+    if player.
